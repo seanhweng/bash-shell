cde269f7c876a29534d1235a664820b610eebb02
seanhweng <29723805+seanhweng@users.noreply.github.com>
2023-02-12 15:38:42 -0600

    Update color_text.sh

--- color_text.sh
+++ color_text.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
 RED="\e[31m"
+# RED="\033[31m"
 GREEN="\e[1;32m"
 # GREEN="\e[33m"
 ENDCOLOR="\e[0m"
+# ENDCOLOR="\033[0m"
 
 echo -e "${RED}This is some red text, ${ENDCOLOR}"
 echo -e "${GREEN}And this is some green text${ENDCOLOR}"
