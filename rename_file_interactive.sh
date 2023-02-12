ecda9200db332a73af3eaa864a527949009f2a29
seanhweng <29723805+seanhweng@users.noreply.github.com>
2023-02-12 16:13:01 -0600

    Update rename_file_interactive.sh

--- rename_file_interactive.sh
+++ rename_file_interactive.sh
@@ -2,10 +2,10 @@
 
 read -p "What type of file extension you want to rename: " file_extension
 
-FILE_LIST=$(ls *.${file_extension} 2>/dev/null)
+file_list=$(ls *.${file_extension} 2>/dev/null)
 DATE=$(date +%F)
 
-if [ -z $FILE_LIST ]
+if [ -z $file_list ]
 then
     echo "No file with extension .${file_extension}"
     exit 1
@@ -15,13 +15,13 @@ read -p "What prefix to use on new file name[default: YYYY-MM-DD]: " PREF
 
 if [ -z $PREF ]
 then
-    for FILE in $FILE_LIST
+    for FILE in $file_list
     do
         echo "Changing ${FILE} to ${DATE}-${FILE}..."
         mv $FILE ${DATE}-${FILE}
     done
 else
-    for FILE in $FILE_LIST
+    for FILE in $file_list
     do
         echo "Changing ${FILE} to ${PREF}-${FILE}..."
         mv $FILE ${PREF}-${FILE}
